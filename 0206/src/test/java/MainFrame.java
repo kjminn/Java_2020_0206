fd1e788fd5c7e85d1a523ec14bca632a56359561
kjminn <kjminn2001@gmail.com>
2020-02-06 20:13:58 +0900

    Add MainFrame

---
+++ 0206/src/test/java/MainFrame.java
@@ -0,0 +1,16 @@
+package test.java;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	
+	public MainFrame() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
